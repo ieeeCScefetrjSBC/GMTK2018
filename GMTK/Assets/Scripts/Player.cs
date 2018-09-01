7c550d954e66fc8fffed60a534b836ebd084378d
phtriplox <phtriplox8090@gmail.com>
2018-08-31 22:41:56 -0300

    Pasta scripts

---
+++ GMTK/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float Vel;
+    Rigidbody2D rb;
+	
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	
+	void Update () {
+        Vector2 vel = Vector2.zero;
+        if (Input.GetKey(KeyCode.W)) vel += Vector2.up;
+        if (Input.GetKey(KeyCode.S)) vel += Vector2.down;
+        if (Input.GetKey(KeyCode.A)) vel += Vector2.left;
+        if (Input.GetKey(KeyCode.D)) vel += Vector2.right;
+
+        rb.velocity = vel.normalized * Vel;
+    }
+}
