c06913687f634e10cbfdec694c580902dc144e30
Rafael Dias <rafunga@gmail.com>
2018-08-31 22:14:31 -0300

    Player basicao

--- GMTK/Assets/Player.cs
+++ GMTK/Assets/Player.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
+    public float Vel;
+    Rigidbody2D rb;
+	
 	void Start () {
-		
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
-	// Update is called once per frame
+	
 	void Update () {
-		
-	}
+        Vector2 vel = Vector2.zero;
+        if (Input.GetKey(KeyCode.W)) vel += Vector2.up;
+        if (Input.GetKey(KeyCode.S)) vel += Vector2.down;
+        if (Input.GetKey(KeyCode.A)) vel += Vector2.left;
+        if (Input.GetKey(KeyCode.D)) vel += Vector2.right;
+
+        rb.velocity = vel.normalized * Vel;
+    }
 }
