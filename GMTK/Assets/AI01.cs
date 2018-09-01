492a8833ade4c9d455cd3f4ec8e663f251a53ead
Rafael Dias <rafunga@gmail.com>
2018-08-31 22:58:44 -0300

    1

--- GMTK/Assets/AI01.cs
+++ GMTK/Assets/AI01.cs
@@ -2,26 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AI01 : Inimigo {
+public class AI01 : Inimigo
+{
 
     Collider2D shortAtack;
     Collider2D mediumAtack;
     Collider2D longAtack;
 
-	void Start () {
+    void Start()
+    {
         Collider2D[] Atacks = GetComponentsInChildren<Collider2D>();
-        foreach (Collider2D at in Atacks) {
+        foreach (Collider2D at in Atacks)
+        {
             if (at.tag == "Short") shortAtack = at;
             if (at.tag == "Medium") mediumAtack = at;
             if (at.tag == "Long") longAtack = at;
         }
-	}
-	
-	void Update () {
-		
-	}
+    }
 
-    private Vector2 Follow() {
+    void Update()
+    {
+
+    }
+
+    private Vector2 Follow()
+    {
         return (((Vector2)Player.Instance.transform.position) - ((Vector2)transform.position)).normalized;
     }
 }
